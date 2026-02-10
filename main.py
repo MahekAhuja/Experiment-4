c6160ba9c7cd39bee20280be96960fc09ed65896
Mahek Ahuja <ahujamahek29@gmail.com>
2026-02-10 11:37:50 +0530

    Merged all branches.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#calculator 
+#calculator
+
+import addition as a 
+import subtraction as s
+import multiplication as m
+
+print("Addition Result  = ",  a.add(5,6))
+print("subtraction Result = ", s.sub(5,6))
+print("multiplication Result = ",m.mul(5,6)) 
